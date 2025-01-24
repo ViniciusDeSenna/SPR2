6c0c04eaac3d5ee43e2c3cfe54d9a374340c9d27
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-01-24 20:03:47 -0300

    Dash board padrao do laravel removido

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen d-flex flex-column justify-content-center align-items-center pt-6 sm:pt-0 bg-light">
             <a href="/">
-                <x-application-logo/>
+                {{-- <x-application-logo/> --}}
             </a>
             <div class="w-100 sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
                 {{ $slot }}
